cf5c534f8b7ad17e1b5bbfee49e3ac47a524f27a
Dan Martin <dmartin3@gmail.com>
2018-07-12 21:22:29 -0400

    Updated to .NET Core 2.1.3

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CityInfo.API {
@@ -8,6 +7,7 @@ namespace CityInfo.API {
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services) {
+      services.AddMvc();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -16,9 +16,7 @@ namespace CityInfo.API {
         app.UseDeveloperExceptionPage();
       }
 
-      app.Run(async (context) => {
-        await context.Response.WriteAsync("Hello World!");
-      });
+      app.UseMvc();
     }
   }
 }

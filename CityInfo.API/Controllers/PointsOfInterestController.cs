02e3c312a608f759ca86540922918d869347d78d
Martin <Dan.Martin@mrisoftware.com>
2018-07-13 16:59:19 -0400

    Added apis to modify POI data

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers {
@@ -15,7 +17,7 @@ namespace CityInfo.API.Controllers {
       return Ok(city.PointsOfInterest);
     }
 
-    [HttpGet("{cityId}/pointsofinterest/{id}")]
+    [HttpGet("{cityId}/pointsofinterest/{id}", Name = "GetPointOfInterest")]
     public IActionResult GetPointOfInterest(int cityId, int id) {
       var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
 
@@ -30,6 +32,121 @@ namespace CityInfo.API.Controllers {
       }
 
       return Ok(pointOfInterest);
+    }
+
+    [HttpPost("{cityId}/pointsofinterest")]
+    public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest) {
+      if (pointOfInterest == null || !ModelState.IsValid) {
+        return BadRequest(ModelState);
+      }
+
+      if (pointOfInterest.Description == pointOfInterest.Name) {
+        ModelState.AddModelError("Description", "The provided description should be different from the name.");
+        return BadRequest(ModelState);
+      }
+
+      var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+
+      if (city == null) {
+        return NotFound();
+      }
+
+      var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(x => x.PointsOfInterest).Max(x => x.Id);
+      var newPointOfInterest = new PointOfInterestDto {
+        Id = ++maxPointOfInterestId,
+        Name = pointOfInterest.Name,
+        Description = pointOfInterest.Description
+      };
+
+      city.PointsOfInterest.Add(newPointOfInterest);
+
+      return CreatedAtRoute("GetPointOfInterest", new {cityId, id = newPointOfInterest.Id}, newPointOfInterest);
+    }
+
+    [HttpPut("{cityId}/pointsofinterest/{id}")]
+    public IActionResult UpdatePointsOfInterest(int cityId, int id,
+      [FromBody] PointOfInterestForUpdateDto pointOfInterest) {
+      if (pointOfInterest == null || !ModelState.IsValid) {
+        return BadRequest(ModelState);
+      }
+
+      if (pointOfInterest.Description == pointOfInterest.Name) {
+        ModelState.AddModelError("Description", "The provided description should be different from the name.");
+        return BadRequest(ModelState);
+      }
+
+      var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+      if (city == null) {
+        return NotFound();
+      }
+
+      var existingPointOfInterest = city.PointsOfInterest.FirstOrDefault(x => x.Id == id);
+      if (existingPointOfInterest == null) {
+        return NotFound();
+      }
+
+      existingPointOfInterest.Name = pointOfInterest.Name;
+      existingPointOfInterest.Description = pointOfInterest.Description;
+
+      return NoContent();
+    }
+
+    [HttpPatch("{cityId}/pointsofinterest/{id}")]
+    public IActionResult PartiallyUpdatePointsOfInterest(int cityId, int id,
+      [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDocument) {
+      if (patchDocument == null) {
+        return BadRequest();
+      }
+
+      var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+      if (city == null) {
+        return NotFound();
+      }
+
+      var existingPointOfInterest = city.PointsOfInterest.FirstOrDefault(x => x.Id == id);
+      if (existingPointOfInterest == null) {
+        return NotFound();
+      }
+
+      var pointOfInterestToPatch = new PointOfInterestForUpdateDto {
+        Name = existingPointOfInterest.Name,
+        Description = existingPointOfInterest.Description
+      };
+
+      patchDocument.ApplyTo(pointOfInterestToPatch, ModelState);
+      if (!ModelState.IsValid) {
+        return BadRequest(ModelState);
+      }
+
+      if (pointOfInterestToPatch.Description == pointOfInterestToPatch.Name) {
+        ModelState.AddModelError("Description", "The provided description should be different from the name.");
+      }
+
+      TryUpdateModelAsync(pointOfInterestToPatch);
+      if (!ModelState.IsValid) {
+        return BadRequest(ModelState);
+      }
+
+      existingPointOfInterest.Name = pointOfInterestToPatch.Name;
+      existingPointOfInterest.Description = pointOfInterestToPatch.Description;
+
+      return NoContent();
+    }
+
+    [HttpDelete("{cityId}/pointsofinterest/{id}")]
+    public IActionResult DeletePointOfInterest(int cityId, int id) {
+      var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+      if (city == null) {
+        return NotFound();
+      }
+
+      var existingPointOfInterest = city.PointsOfInterest.FirstOrDefault(x => x.Id == id);
+      if (existingPointOfInterest == null) {
+        return NotFound();
+      }
+
+      city.PointsOfInterest.Remove(existingPointOfInterest);
+      return NoContent();
     }
   }
 }
---
+++ CityInfo.API/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SG0016:Controller method is vulnerable to CSRF", Justification = "<Pending>", Scope = "member", Target = "~M:CityInfo.API.Controllers.PointsOfInterestController.UpdatePointsOfInterest(System.Int32,System.Int32,CityInfo.API.Models.PointOfInterestForUpdateDto)~Microsoft.AspNetCore.Mvc.IActionResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SG0016:Controller method is vulnerable to CSRF", Justification = "<Pending>", Scope = "member", Target = "~M:CityInfo.API.Controllers.PointsOfInterestController.PartiallyUpdatePointsOfInterest(System.Int32,System.Int32,CityInfo.API.Models.PointOfInterestForUpdateDto)~Microsoft.AspNetCore.Mvc.IActionResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SG0016:Controller method is vulnerable to CSRF", Justification = "<Pending>", Scope = "member", Target = "~M:CityInfo.API.Controllers.PointsOfInterestController.PartiallyUpdatePointsOfInterest(System.Int32,System.Int32,Microsoft.AspNetCore.JsonPatch.JsonPatchDocument{CityInfo.API.Models.PointOfInterestForUpdateDto})~Microsoft.AspNetCore.Mvc.IActionResult")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SG0016:Controller method is vulnerable to CSRF", Justification = "<Pending>", Scope = "member", Target = "~M:CityInfo.API.Controllers.PointsOfInterestController.DeletePointOfInterest(System.Int32,System.Int32)~Microsoft.AspNetCore.Mvc.IActionResult")]
+
